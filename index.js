cf07b066731aa932f2e1ebe1d0eadfc912155b41
Konrad Pabjan <Konrad.Pabjan@microsoft.com>
2019-10-22 13:24:38 -0400

    Test deployment api

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const github = require("@actions/github");
 const core = require("@actions/core");
 
 async function run() {
+    const myToken = core.getInput("action-token");
+    const octokit = new github.GitHub(myToken);
+    const context = github.context;
+
     return "Ayoo this works!";
+
+    var info = {
+        auto_merge: false,
+        description: "This is a test of a deployment", 
+        environment: "production",
+        owner: context.payload.owner,
+        ref: context.payload.pull_request.ref,
+        repo: context.payload.repo
+    };
+
+    return await octokit.repos.createDeployment(info);
 }
 
 run()
